30a60d2e57542d7b108f6343178a94e143b751df
David Ball <drb128@hotmail.com>
2019-03-18 17:12:53 -0700

    adding javascript and html index files

---
+++ draw.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", function()
+{
+    var image  = document.getElementById("html5");
+    var canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+
+    canvas.width  = image.width;
+    canvas.height = image.height;
+
+    var context = canvas.getContext("2d");
+
+    context.drawImage(image, 0, 0);
+});
